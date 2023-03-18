8f8a04ef834b653c0697173299215fb0b5bfb6ae
Nadiia Tsytsylina <miniova95@gmail.com>
2023-03-18 16:11:54 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,16 +2,12 @@
 // подставляет его текущее значение в span#name - output.Если инпут пустой, в спане должна отображаться строка "Anonymous".
 
 const textInput = document.querySelector('#name-input');
-console.log(textInput);
-
 const output = document.querySelector('#name-output');
-console.log(output);
 
 textInput.addEventListener('input', (event) => {
   if (event.currentTarget.value === '') {
     output.textContent = 'Anonymus';
   } else {
     output.textContent = event.currentTarget.value;
-    console.log(event.currentTarget.value);
   }
 });
